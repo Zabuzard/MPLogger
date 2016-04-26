470b5289f90e20f7a2c3947b477712bf76bf8f69
Daniel Tischner <Slippy-Toad@web.de>
2016-04-26 11:35:13 +0200

    Send location-messages only once a day per world

--- \trunk/source/MPLogger/sendMessage.php
+++ \trunk/source/MPLogger/sendMessage.php
@@ -1,6 +1,23 @@
 <?php
 function sendMessage($world, $option) {
+	global $linkLogs;
+	
 	//If true bot does not stand at correct location, if false bot has a message
+	if ($option) {
+		// Only send those messages if not already done today
+		$query = "SELECT `ts` FROM `messages` WHERE `world` = '$world' AND `ts` > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 1 DAY)";
+		$res = mysql_query($query, $linkLogs);
+		echo $res.mysql_error()."s<br />";
+		$out = mysql_fetch_assoc($res);
+		if ($out == "") {
+			// Decide to send the message
+			$query = "INSERT INTO `zabuza`.`messages` (`world`, `ts`) VALUES ('$world', CURRENT_TIMESTAMP);";
+			echo mysql_query($query, $linkLogs).mysql_error()."i <br />";
+		} else {
+			// There was already a message send today, abort.
+			return;
+		}
+	}
 	$message = "";
 	if ($option) {
 		$message = "Freewar - Marktpreisbot\nBot aus Welt $world steht nicht auf der Markthalle.";
